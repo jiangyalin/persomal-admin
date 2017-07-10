f56a848fbc2e14a56b2c771902611389077ac1ce
jiangyalin <18725944157@163.com>
2017-07-10 14:41:50 +0800

    xx

---
+++ routes/index.js
@@ -0,0 +1,37 @@
+const moment = require('moment');
+const bodyParser = require('body-parser');
+
+module.exports = function (app) {
+
+    app.use(bodyParser.urlencoded({extended: false}));
+    app.use(bodyParser.json());//解析参数
+
+    app.get('/',function (req, res) {
+        return res.redirect('/index');
+    });
+
+    //登陆拦截
+    app.use(function(req,res,next){
+        if (!req.session.UrlReferrer) {
+            req.session.UrlReferrer = '/index';//记录用户的访问地址
+        }
+        if (req.originalUrl.indexOf('/login') == -1 && req.originalUrl.indexOf('.') == -1) {
+            req.session.UrlReferrer = req.originalUrl;//记录用户的访问地址
+            if (!req.session.user) {
+                res.redirect('/login');
+            }
+        }
+        return next();
+    });
+
+    //首页
+    app.get('/index',function (req, res) {
+        res.render('index');
+    });
+
+    //登陆
+    app.use('/login',require("./login/index"));
+    //文档管理
+    app.use('/article',require("./article/index"));
+
+};
