976600b27d7bc40ddd9ee135460ed26a7d7ce5c0
jiangyalin <18725944157@163.com>
2017-09-04 15:23:43 +0800

    配置

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use(session({
     resave: true,// 强制更新 session
     saveUninitialized: false,// 设置为 false，强制创建一个 session，即使用户未登录
     cookie: {
+        httpOnly: false,
         maxAge: config.session.maxAge// 过期时间，过期后 cookie 中的 session id 自动删除
     },
     store: new MongoStore({// 将 session 存储到 mongodb
